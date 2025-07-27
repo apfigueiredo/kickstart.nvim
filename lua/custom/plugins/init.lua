01e5217cb8048ea235119f0c2407e9df08f0976a
Aloysio Figueiredo <aloysio@gmail.com>
2025-07-27 12:13:55 -0300

    adding neorg

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,6 +11,20 @@ return {
     'sindrets/diffview.nvim',
     'nvim-lua/plenary.nvim',
   } },
+  {
+    'nvim-neorg/neorg',
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = '*', -- Pin Neorg to the latest stable release
+    config = true,
+    opts = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {}, -- Load all the default modules
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+        },
+      }
+    end,
+  },
   { 'akinsho/toggleterm.nvim', version = '*', opts = {} },
   {
     'yetone/avante.nvim',
