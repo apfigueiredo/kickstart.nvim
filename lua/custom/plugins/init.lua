578cbe0f405cb0d45fa1e1d66e6de4a08c6cb31c
Aloysio Figueiredo <aloysio@gmail.com>
2025-07-27 23:43:21 -0300

    remove obsidian

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,11 +14,7 @@ return {
   { 'akinsho/toggleterm.nvim', version = '*', opts = {} },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     opts = function()
       require('render-markdown').setup {
         completions = { lsp = { enabled = true } },
@@ -125,35 +121,6 @@ return {
           collapsed = true,
           open = 'Neotree position=top document_symbols',
         },
-      },
-    },
-  },
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*',
-    lazy = false,
-    ft = 'markdown',
-    opts = {
-      workspaces = {
-        {
-          name = 'personal',
-          path = '~/obsidian/',
-        },
-      },
-      new_notes_location = 'Neovim/Inbox',
-    },
-    {
-      'folke/flash.nvim',
-      event = 'VeryLazy',
-      ---@type Flash.Config
-      opts = {},
-  -- stylua: ignore
-      keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
       },
     },
   },
