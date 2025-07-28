3dc7f9aa760d1be04d3340f078a169dde1c661e3
Aloysio Figueiredo <aloysio@gmail.com>
2025-07-27 21:09:16 -0300

    add markdown plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,6 +13,19 @@ return {
   } },
   { 'akinsho/toggleterm.nvim', version = '*', opts = {} },
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = function()
+      require('render-markdown').setup {
+        completions = { lsp = { enabled = true } },
+      }
+    end,
+  },
+  {
     'yetone/avante.nvim',
     build = function()
       return 'make'
